cf35c1f93ad9025994d37663364a62277a24f446
Darek Finster <darek.finster@gmail.com>
2013-04-25 12:10:41 +0200

    Set headers for request

--- helpers/main.rb
+++ helpers/main.rb
@@ -1,8 +1,12 @@
 helpers do
   def root_url
-    server_name = headers['SERVER_NAME'] || 'localhost:4567'
+    server_name = request_headers['host'] || 'localhost:4567s'
     'http://' + server_name
   end
+
+  def request_headers
+    env.inject({}){|acc, (k,v)| acc[$1.downcase] = v if k =~ /^http_(.*)/i; acc}
+  end  
 
   # Escape URIs
   def u(text)
